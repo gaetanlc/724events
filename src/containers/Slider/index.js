c79be2c0c8edbb59763245e376ad6100aa617c60
Gaetan Le Corre <lecorre.gaetan1@gmail.com>
2026-04-21 15:20:04 +0200

    Fix console warnings: Logo CSS, Slider optional chaining, EventCard conditional render

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -7,49 +7,50 @@ import "./style.scss";
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
-  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+  const byDateDesc = data?.focus?.sort((evtA, evtB) =>
     new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
   );
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length - 1 ? index + 1 : 0),
+      () => setIndex(index < (byDateDesc?.length ?? 0) - 1 ? index + 1 : 0),
       5000
     );
   };
   useEffect(() => {
     nextCard();
   });
-return (
-  <div className="SlideCardList">
-    {byDateDesc?.map((event, idx) => (
-      <div
-        key={event.title}
-        className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}
-      >
-        <img src={event.cover} alt="forum" />
-        <div className="SlideCard__descriptionContainer">
-          <div className="SlideCard__description">
-            <h3>{event.title}</h3>
-            <p>{event.description}</p>
-            <div>{getMonth(new Date(event.date))}</div>
+  return (
+    <div className="SlideCardList">
+      {byDateDesc?.map((event, idx) => (
+        <div
+          key={event.title}
+          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"}`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
+            </div>
           </div>
         </div>
-      </div>
-    ))}
-    <div className="SlideCard__paginationContainer">
-      <div className="SlideCard__pagination">
-        {byDateDesc?.map((_, radioIdx) => (
-          <input
-            key={radioIdx} // eslint-disable-line react/no-array-index-key
-            type="radio"
-            name="radio-button"
-            checked={index === radioIdx}
-          />
-        ))}
+      ))}
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((_, radioIdx) => (
+            <input
+              key={radioIdx} // eslint-disable-line react/no-array-index-key
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              onChange={() => setIndex(radioIdx)}
+            />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 };
 
 export default Slider;
